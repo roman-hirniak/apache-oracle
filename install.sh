08c423676ef4892a96dc18138f7520b4410cd70c
roman-hirniak <8720580+roman-hirniak@users.noreply.github.com>
2022-11-19 13:10:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ sudo systemctl enable --now httpd.service
 sudo systemctl status httpd
 sudo firewall-cmd --add-service=http --permanent
 sudo firewall-cmd --reload
+sudo chmod 666 /var/www/html/index.html
+echo "Roman Hirniak" >> /var/www/html/index.html
